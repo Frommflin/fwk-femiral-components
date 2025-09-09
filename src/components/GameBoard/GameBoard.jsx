a80cfb36c74f96db536eb55781ac50833e75171c
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-09-09 10:19:51 +0200

    Gameboard comp

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import styles from "./GameBoard.module.css";
 
-export const GameBoard = ({ size = 7 }) => {
+export const GameBoard = ({
+  size = 7,
+  coloredCells = [{ i: 5, color: "white" }],
+}) => {
   return (
     <div className={styles.gameBoard}>
-      {Array.from({ length: size * size }, (_, i) => (
-        <div key={i} className={styles.cell}></div>
-      ))}
+      {Array.from({ length: size * size }, (_, i) => {
+        const found = coloredCells.find((c) => c.i === i);
+        const color = found ? found.color : "white";
+
+        return (
+          <div key={i} className={styles.cell}>
+            <div
+              className={styles.cellContent}
+              style={{ backgroundColor: color }}
+            >
+              {i}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
