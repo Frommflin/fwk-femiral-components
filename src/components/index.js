525134ce41a90ed60e72a7e9d3e41c0d166aa529
Klara Frommelin <freakyfrommelin@outlook.com>
2025-09-19 10:55:05 +0200

    fixing missed export adjustment

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ export { GameBoard } from "./GameBoard/GameBoard";
 export { GameBoard2 } from "./GameBoard2/GameBoard2";
 export { Header } from "./Header/Header";
 export { PlayerCard } from "./PlayerCard/PlayerCard";
-export { GameSquare } from "./GameSquare/GameSquare";
+export { GameSquare } from "./GameSquare/GameSquare.molecule";
 export { GameTile } from "./GameTile/GameTile.atom";
 export { Input } from "./Input/Input";
 export { Login } from "./Login/Login";
