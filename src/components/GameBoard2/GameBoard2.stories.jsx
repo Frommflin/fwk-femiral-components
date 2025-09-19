0a447c41e553be7fc17818df5003fc493431c948
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-09-19 09:31:56 +0200

    gameboard2 css and first storybook story

---
+++ src/components/GameBoard2/GameBoard2.stories.jsx
@@ -0,0 +1,12 @@
+import GameBoard2 from "./GameBoard2";
+
+export default {
+  title: "Components/GameBoard2",
+  component: GameBoard2,
+};
+
+export const Default = {
+  args: {
+    size: 7,
+  },
+};
