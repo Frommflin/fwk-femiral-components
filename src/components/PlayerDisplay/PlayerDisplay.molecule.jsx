c04481b2ce30100d6d4f570573d42a8dcf60f64c
Klara Frommelin <freakyfrommelin@outlook.com>
2025-09-22 11:44:53 +0200

    Building playerDisplay component

--- src/components/PlayerDisplay/PlayerDisplay.molecule.jsx
+++ src/components/PlayerDisplay/PlayerDisplay.molecule.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import styles from "./PlayerDisplay.module.css";
+import PlayerCard from "../PlayerCard";
 
-const PlayerDisplay = () => {
-  return <div className={styles.container}>PlayerDisplay</div>;
+const PlayerDisplay = ({ user1, user2 }) => {
+  return (
+    <div className={styles.container}>
+      <PlayerCard i="1" user={user1} color="yellow" />
+      <PlayerCard i="2" user={user2} color="red" />
+    </div>
+  );
 };
 
 export default PlayerDisplay;
