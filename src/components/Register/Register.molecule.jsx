da852c0d2a30175d0d08fb339162f7e10b662873
rozabelay <rozaybeyn.belay@gmail.com>
2025-09-28 21:16:26 +0200

    imported label komponnet to regiter.molocule.jsx component.

--- src/components/Register/Register.molecule.jsx
+++ src/components/Register/Register.molecule.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import Input from '../Input/Input.atom';
 import styles from './Register.module.css';
+import Label from '../Label/Label.atom';
 
 const Register = ({ username, password, onChange }) => {
   return (
     <div className={styles.registerMolecule}>
       <h3>Register</h3>
-
+       <Label text="Username" htmlFor="username" />
       <Input
         type="text"
         value={username}
         onChange={(e) => onChange({ name: "username", value: e.target.value })}
         placeholder="Username"
       />
-
+       <Label text="Password" htmlFor="Password" />
       <Input
         type="password"
         value={password}
