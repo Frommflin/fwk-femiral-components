c36413521a4166331e3b7873882f19cfc8a2351b
Klara Frommelin <freakyfrommelin@outlook.com>
2025-09-29 16:38:42 +0200

    Adjusting Header to atomic design

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 export { GameBoard } from "./GameBoard/GameBoard";
 export { GameBoard2 } from "./GameBoard2/GameBoard2.organism";
-export { Header } from "./Header/Header";
+export { Header } from "./Header/Header.organism";
 export { PlayerCard } from "./PlayerCard/PlayerCard.atom";
 export { GameSquare } from "./GameSquare/GameSquare.molecule";
 export { GameTile } from "./GameTile/GameTile.atom";
