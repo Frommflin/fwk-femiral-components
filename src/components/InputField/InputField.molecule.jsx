57531e96cb424207e0f715fad16bcd8d269c221e
Klara Frommelin <freakyfrommelin@outlook.com>
2025-09-29 15:03:08 +0200

    styling placeholder content using boxmodel

---
+++ src/components/InputField/InputField.molecule.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./InputField.module.css";
+
+const InputField = () => {
+  return <div className={styles.container}>InputField</div>;
+};
+
+export default InputField;
