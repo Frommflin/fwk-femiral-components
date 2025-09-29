e336bf7631c1733510d2114a7da55f06893dc0ef
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-09-29 15:06:50 +0200

    Array of buttons

--- src/components/ButtonRow/ButtonRow.molecule.jsx
+++ src/components/ButtonRow/ButtonRow.molecule.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import styles from "./ButtonRow.module.css";
 
 const ButtonRow = () => {
-  return <div className={styles.container}>ButtonRow</div>;
+  return (
+    <div className={styles.container}>
+      {Array.from({ length: 7 }).map((_, index) => (
+        <button key={index}>Button {index + 1}</button>
+      ))}
+    </div>
+  );
 };
 
 export default ButtonRow;
