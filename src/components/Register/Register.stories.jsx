2cc5f19a3bc7d76b590a819851e5d33e5004c5ba
emiliamailian <emiliamailian@gmail.com>
2025-09-29 10:37:06 +0200

    storybook tillagd för Register

--- src/components/Register/Register.stories.jsx
+++ src/components/Register/Register.stories.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
-import Register from './Register.molecule';
+import React, { useState } from "react";
+import Register from "./Register.molecule";
 
 export default {
-  title: 'Components/Register',
+  title: "Components/Register",
   component: Register,
 };
+
+export const Placeholder = () => <Register showPlaceholder />;
 
 export const Default = () => {
   const [formData, setFormData] = useState({ username: "", password: "" });
