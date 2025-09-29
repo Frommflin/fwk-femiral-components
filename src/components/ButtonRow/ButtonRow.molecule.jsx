cd3f49dd6d55665761bd4cdb23d8eb2fc84d4c67
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-09-29 15:29:04 +0200

    changed to using Button component in ButtonRow

--- src/components/ButtonRow/ButtonRow.molecule.jsx
+++ src/components/ButtonRow/ButtonRow.molecule.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import styles from "./ButtonRow.module.css";
+import { Button } from "..";
 
 const ButtonRow = ({ numberOfButtons, onClick }) => {
   return (
     <div className={styles.container}>
       {Array.from({ length: numberOfButtons }).map((_, index) => (
-        <button key={index} onClick={() => onClick(index)}>
-          Button {index + 1}
-        </button>
+        <Button
+          key={index}
+          onClick={() => onClick(index)}
+          label={`Button ${index + 1}`}
+        />
       ))}
     </div>
   );
