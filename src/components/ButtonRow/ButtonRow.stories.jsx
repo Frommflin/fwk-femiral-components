241982e0b34f715efeaf1aa6d18ba6a4624d6a89
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-09-29 14:41:10 +0200

    first itaration of buttonrow structure setup

---
+++ src/components/ButtonRow/ButtonRow.stories.jsx
@@ -0,0 +1,11 @@
+import ButtonRow from "./ButtonRow.molecule";
+
+export default {
+  title: "Components/ButtonRow",
+  component: ButtonRow,
+  parameters: {
+    layout: "centered",
+  },
+};
+
+export const Default = {};
