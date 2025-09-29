cee1bb0b1b0d42493e6ea6b33c0ea9f0c1a565a1
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-09-29 14:53:54 +0200

    buttenrow second iteration, exports in berral files and implement boxmodel

---
+++ src/components/ButtonRow/ButtonRow.molecule.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./ButtonRow.module.css";
+
+const ButtonRow = () => {
+  return <div className={styles.container}>ButtonRow</div>;
+};
+
+export default ButtonRow;
