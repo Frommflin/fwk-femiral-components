0735f0a5e61323dc0e6fbc0786a3d1229ab27003
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-09-29 15:27:25 +0200

    styling added attrebute onclick

--- src/components/ButtonRow/ButtonRow.molecule.jsx
+++ src/components/ButtonRow/ButtonRow.molecule.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./ButtonRow.module.css";
 
-const ButtonRow = ({ numberOfButtons }) => {
+const ButtonRow = ({ numberOfButtons, onClick }) => {
   return (
     <div className={styles.container}>
       {Array.from({ length: numberOfButtons }).map((_, index) => (
-        <button key={index}>Button {index + 1}</button>
+        <button key={index} onClick={() => onClick(index)}>
+          Button {index + 1}
+        </button>
       ))}
     </div>
   );
