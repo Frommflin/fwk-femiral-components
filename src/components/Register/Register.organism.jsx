c4fc2b6a39f65cd1a75375ead56c390d1f1d7678
rozabelay <rozaybeyn.belay@gmail.com>
2025-09-29 12:36:24 +0200

    changed the register.molocule to organism, imported button component and modfied register.stories.jsx accorging that

--- src/components/Register/Register.organism.jsx
+++ src/components/Register/Register.organism.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import Input from '../Input/Input.atom';
 import styles from "./Register.module.css";
 import Label from '../Label/Label.atom';
+import Button from '../Button/Button.atom';
 
+<<<<<<< HEAD:src/components/Register/Register.molecule.jsx
 const Register = ({ username, password, onChange, showPlaceholder = false }) => {
   if (showPlaceholder) return <div className={styles.registerMolecule}>box</div>;
 
+=======
+const Register = ({ username, password, onChange, onSubmit }) => {
+>>>>>>> b29b5c1 (changed the register.molocule to organism, imported button component and modfied register.stories.jsx accorging that):src/components/Register/Register.organism.jsx
   return (
     <div className={styles.registerMolecule}>
       <h3>Register</h3>
@@ -23,7 +28,13 @@ const Register = ({ username, password, onChange, showPlaceholder = false }) => 
         onChange={(e) => onChange({ name: "password", value: e.target.value })}
         placeholder="Password"
       />
+      <Button
+        type="submit"
+        label="Register"
+        onClick={onSubmit}
+      />
     </div>
+    
   );
 };
 
--- src/components/Register/Register.stories.jsx
+++ src/components/Register/Register.stories.jsx
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 import React, { useState } from "react";
 import Register from "./Register.molecule";
+=======
+import React, { useState } from 'react';
+import Register from './Register.organism';
+>>>>>>> b29b5c1 (changed the register.molocule to organism, imported button component and modfied register.stories.jsx accorging that)
 
 export default {
   title: "Components/Register",
@@ -15,11 +20,16 @@ export const Default = () => {
     setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
+  const handleSubmit = () => {
+    alert(`Username: ${formData.username}, Password: ${formData.password}`);
+  };
+
   return (
     <Register
       username={formData.username}
       password={formData.password}
       onChange={handleChange}
+      onSubmit={handleSubmit}
     />
   );
 };
