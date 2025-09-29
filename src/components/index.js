bd92e0bc2e83a4488ffa42741eddf2e7a79c7e6b
Klara Frommelin <freakyfrommelin@outlook.com>
2025-09-29 14:08:27 +0200

    fixing faulty Input exports

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,7 @@ export { Header } from "./Header/Header";
 export { PlayerCard } from "./PlayerCard/PlayerCard.atom";
 export { GameSquare } from "./GameSquare/GameSquare.molecule";
 export { GameTile } from "./GameTile/GameTile.atom";
-export { Input } from "./Input/Input";
+export { Input } from "./Input/Input.atom";
 export { Login } from "./Login/Login";
 export { PlayerDisplay } from "./PlayerDisplay/PlayerDisplay.molecule";
 export { Button } from "./Button/Button.atom";
