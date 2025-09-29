898bfbba417f4e8075f824b396f0f4f11cceb1b5
Klara Frommelin <freakyfrommelin@outlook.com>
2025-09-29 16:33:10 +0200

    writing storybook tests for InputField

--- src/components/InputField/InputField.stories.jsx
+++ src/components/InputField/InputField.stories.jsx
@@ -5,4 +5,35 @@ export default {
   component: InputField,
 };
 
-export const Default = {};
+export const TextInput = {
+  args: {
+    inputId: "text1",
+    label: "Text Input",
+    type: "text",
+    onChange: () => console.log("Value has been changed"),
+    placeholder: "This is a text input",
+    disabled: false,
+  },
+};
+
+export const PasswordInput = {
+  args: {
+    inputId: "pwd1",
+    label: "Password Input",
+    type: "password",
+    onChange: () => console.log("Value has been changed"),
+    placeholder: "This is a password input",
+    disabled: false,
+  },
+};
+
+export const DisabledTextInput = {
+  args: {
+    inputId: "text2",
+    label: "Disabled Input",
+    type: "text",
+    onChange: () => {},
+    placeholder: "This is a disabled input",
+    disabled: true,
+  },
+};
