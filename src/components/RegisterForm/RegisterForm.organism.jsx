70c3df89000200d3fb5529527f66744e7b4fd212
Klara Frommelin <freakyfrommelin@outlook.com>
2025-10-01 23:41:53 +0200

    adjusting RegisterForm props and tests

--- src/components/RegisterForm/RegisterForm.organism.jsx
+++ src/components/RegisterForm/RegisterForm.organism.jsx
@@ -1,36 +1,55 @@
-import React from "react";
-import Input from '../InputField/InputField.molecule'
+import React, { useState } from "react";
+import Input from "../InputField/InputField.molecule";
 import styles from "./RegisterForm.module.css";
-import Button from '../Button/Button.atom';
+import Button from "../Button/Button.atom";
 
+const RegisterForm = ({ onSubmit }) => {
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+  const onNameChange = (name) => {
+    setUser({
+      ...user,
+      username: name,
+    });
+  };
+  const onPwdChange = (pwd) => {
+    setUser({
+      ...user,
+      password: pwd,
+    });
+  };
 
-const RegisterForm = ({ username, password, onChange, onSubmit }) => {
   return (
     <div className={styles.registerOrganism}>
-      <h3>Register form</h3>
+      <h3>Register account</h3>
       <Input
+        inputId="username"
         label="Username"
         type="text"
-        value={username}
-        onChange={(e) => onChange({ name: "username", value: e.target.value })}
+        onChange={(e) => onNameChange(e.target.value)}
         placeholder="Username"
       />
-     
+
       <Input
-      label="Password"
+        inputId="pwd"
+        label="Password"
         type="password"
-        value={password}
-        onChange={(e) => onChange({ name: "password", value: e.target.value })}
+        onChange={(e) => onPwdChange(e.target.value)}
         placeholder="Password"
       />
+
       <Button
         type="submit"
         label="Register"
-        onClick={onSubmit}
+        onClick={(e) => {
+          e.preventDefault();
+          onSubmit(user);
+        }}
         className={styles.registerButton}
       />
     </div>
-    
   );
 };
 
--- src/components/RegisterForm/RegisterForm.stories.jsx
+++ src/components/RegisterForm/RegisterForm.stories.jsx
@@ -1,31 +1,15 @@
-
-import React, { useState } from 'react';
-import RegisterForm from './RegisterForm.organism';
+import React from "react";
+import RegisterForm from "./RegisterForm.organism";
 
 export default {
   title: "Components/RegisterForm",
   component: RegisterForm,
 };
 
-export const Placeholder = () => <RegisterForm showPlaceholder />;
-
 export const Default = () => {
-  const [formData, setFormData] = useState({ username: "", password: "" });
-
-  const handleChange = ({ name, value }) => {
-    setFormData((prev) => ({ ...prev, [name]: value }));
+  const onSubmit = (user) => {
+    alert(`Username: ${user.username}, Password: ${user.password} `);
   };
 
-  const handleSubmit = () => {
-    alert(`Username: ${formData.username}, Password: ${formData.password}`);
-  };
-
-  return (
-    <RegisterForm
-      username={formData.username}
-      password={formData.password}
-      onChange={handleChange}
-      onSubmit={handleSubmit}
-    />
-  );
+  return <RegisterForm onSubmit={onSubmit} />;
 };
