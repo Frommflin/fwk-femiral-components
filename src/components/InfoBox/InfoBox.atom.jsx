31856cce5311d058e7a70b60f33245d5a36886c3
Klara Frommelin <freakyfrommelin@outlook.com>
2025-10-01 10:39:04 +0200

    Writing storybook tests for InfoBox

--- src/components/InfoBox/InfoBox.atom.jsx
+++ src/components/InfoBox/InfoBox.atom.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./InfoBox.module.css";
 
-const InfoBox = ({ styling, children }) => {
+const InfoBox = ({ styling = {}, children }) => {
   return (
     <div className={styles.container} style={styling}>
       {children}
--- src/components/InfoBox/InfoBox.stories.jsx
+++ src/components/InfoBox/InfoBox.stories.jsx
@@ -6,3 +6,34 @@ export default {
 };
 
 export const Default = {};
+
+export const WithChild = () => {
+  return (
+    <InfoBox>
+      <p>This is a paragraph child</p>
+    </InfoBox>
+  );
+};
+
+export const WithChildren = () => {
+  const styleOverride = {
+    width: "500px",
+    height: "200px",
+    backgroundColor: "#2c2c2c",
+    color: "lightblue",
+    padding: "0 20px",
+  };
+  return (
+    <InfoBox styling={styleOverride}>
+      <h1>Title child</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo earum
+        dolores exercitationem veritatis, aspernatur possimus. Quidem nihil quae
+        rerum tenetur id alias rem aut! Recusandae facilis necessitatibus
+        debitis, id atque quam magnam! Rerum, facere molestias voluptas
+        praesentium dolorum, ipsam consectetur, saepe exercitationem nulla ex
+        soluta vel a ipsa tempora cum!
+      </p>
+    </InfoBox>
+  );
+};
