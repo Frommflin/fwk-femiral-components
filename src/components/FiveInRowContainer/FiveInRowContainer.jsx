df3adeee8b20652c7ea7b86afe887c62f17f03e9
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-10-01 20:08:29 +0200

    first itaration FiveInRowContainer

---
+++ src/components/FiveInRowContainer/FiveInRowContainer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FiveInRowContainer = () => {
+  return <div>FiveInRowContainer</div>;
+};
+
+export default FiveInRowContainer;
---
+++ src/components/FiveInRowContainer/FiveInRowContainer.stories.jsx
@@ -0,0 +1,10 @@
+import FiveInRowContainer from "./FiveInRowContainer";
+
+export default {
+  title: "FiveInRowContainer",
+  component: FiveInRowContainer,
+};
+
+const Default = {
+  args: {},
+};
